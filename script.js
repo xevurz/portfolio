6bc6351ed25fbf5fc6535d2d44466e5b3caf5c27
xevurz <ravengrande0@gmail.com>
2026-05-15 20:12:26 +0800

    add fade up adnimations

--- script.js
+++ script.js
@@ -83,3 +83,17 @@ window.addEventListener("scroll", () => {
         }
     });
 });
+
+const fadeElements = document.querySelectorAll(".fade-up");
+
+window.addEventListener("scroll", () => {
+
+    fadeElements.forEach(el => {
+
+        const top = el.getBoundingClientRect().top;
+
+        if(top < window.innerHeight - 100){
+            el.classList.add("show");
+        }
+    });
+});
