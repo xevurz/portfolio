14db81e7627ac048b4d956d9273c33c7c2c0eaa2
xevurz <ravengrande0@gmail.com>
2026-05-15 20:01:11 +0800

    nav response on scrolling

--- script.js
+++ script.js
@@ -57,3 +57,29 @@ function typeEffect(){
 }
 
 typeEffect();
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".tab");
+
+window.addEventListener("scroll", () => {
+
+    let current = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 200;
+
+        if(pageYOffset >= sectionTop){
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === `#${current}`){
+            link.classList.add("active");
+        }
+    });
+});
